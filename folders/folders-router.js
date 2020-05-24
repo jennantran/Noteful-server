a61e3fb7723d61bea13e1293ebed5b6adf8e87d8
Jennifer Tran <jennantran@gmail.com>
2020-05-24 13:19:49 -0700

    added delete folder

--- folders/folders-router.js
+++ folders/folders-router.js
@@ -17,7 +17,7 @@ foldersRouter
             req.app.get('db')
         )
         .then(folders => {
-            res.json(folders)
+            res.json(folders.map(serializeFolders))
         })
         .catch(next)
     })
@@ -39,11 +39,40 @@ foldersRouter
             .then( folder => {
                 res
                     .status(201)
-                    .json(folder)
+                    .json(serializeFolder(folder))
             })
             .catch(next)
         });
 
+    foldersRouter
+        .route('/:folder_id')
+        .all((req,res,next) => {
+            FoldersService.getById('db', req.params.folder_id)
+                .then(folder => {
+                    if(!folder){
+                        return res.status(404).json({
+                            error: { message: `Folder doesn't exist` }
+                        })
+                    }
+                    res.folder = folder
+                    next()
+                })
+                .catch(next)
+        })
+        .get((req,res,next) => {
+            res.json(serializeFolder(res.folder))
+        })
+        .delete((req,res,next) => {
+            FoldersService.deleteService(
+                req.app.get('db'),
+                req.params.folder_id
+            )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+        })
+        
 module.exports(artilesRouter)
    
 
